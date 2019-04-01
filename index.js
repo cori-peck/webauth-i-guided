a3d101dd89e7656bf7c68093e5ef11927e1a6eda
cori-peck <cori.peck@ymail.com>
2019-04-01 13:22:46 -0500

    working guided exercise

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs')
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
@@ -18,6 +19,10 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
 
+  //hash the password
+  const hash = bcrypt.hashSync(user.password, 4);
+  user.password = hash;
+
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -33,7 +38,8 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      //check the password guess against the database
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -44,13 +50,45 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+//restrict access to this endpoint to only users that prov the right credentials in the headers
+server.get('/api/users', restricted, only('frodo'), (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+function restricted(req, res, next) {
+ const { username, password } = req.headers;
+if (username && password) {
+ Users.findBy({ username })
+ .first()
+ .then(user => {
+   if (user && bcrypt.compareSync(password, user.password)) {
+     next();
+   } else {
+     res.status(401).json({ message: 'You Shall Not Pass!' });
+   }
+ })
+  .catch(error => {
+   res.status(500).json(error);
+  });
+  } else {
+    res.status(401).json({ message: "Please Provide Crendtials"});
+  }
+}
+
+function only(username) {
+  return function(req, res, next) {
+    if (req.headers.username === username) {
+      next()
+    } else {
+      res.status(403).json({ message: `You are not ${username}` });
+    }
+  }
+}
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
